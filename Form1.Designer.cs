5a71bd8e283bf6151f745fc56e67002eba145330
Chuck Kowalewski <drchuck@hotmail.com>
2020-06-17 23:22:59 -0400

    Version 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,103 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.ReadSCTButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new SCTColorManager.DataSet1();
+            this.sCTdefineBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.WriteFileButton = new System.Windows.Forms.Button();
+            this.InstructionLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sCTdefineBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ReadSCTButton
+            // 
+            this.ReadSCTButton.Location = new System.Drawing.Point(27, 34);
+            this.ReadSCTButton.Name = "ReadSCTButton";
+            this.ReadSCTButton.Size = new System.Drawing.Size(116, 23);
+            this.ReadSCTButton.TabIndex = 0;
+            this.ReadSCTButton.Text = "Read SCT file";
+            this.ReadSCTButton.UseVisualStyleBackColor = true;
+            this.ReadSCTButton.Click += new System.EventHandler(this.ReadSCTButton_Click);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AnyColor = true;
+            this.colorDialog1.FullOpen = true;
+            this.colorDialog1.SolidColorOnly = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 93);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 315);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sCTdefineBindingSource
+            // 
+            this.sCTdefineBindingSource.DataMember = "SCTdefine";
+            this.sCTdefineBindingSource.DataSource = this.dataSet1;
+            // 
+            // WriteFileButton
+            // 
+            this.WriteFileButton.Location = new System.Drawing.Point(150, 34);
+            this.WriteFileButton.Name = "WriteFileButton";
+            this.WriteFileButton.Size = new System.Drawing.Size(98, 23);
+            this.WriteFileButton.TabIndex = 9;
+            this.WriteFileButton.Text = "Write SCT file";
+            this.WriteFileButton.UseVisualStyleBackColor = true;
+            this.WriteFileButton.Click += new System.EventHandler(this.WriteFileButton_Click);
+            // 
+            // InstructionLabel
+            // 
+            this.InstructionLabel.AutoSize = true;
+            this.InstructionLabel.Location = new System.Drawing.Point(95, 411);
+            this.InstructionLabel.Name = "InstructionLabel";
+            this.InstructionLabel.Size = new System.Drawing.Size(172, 13);
+            this.InstructionLabel.TabIndex = 10;
+            this.InstructionLabel.Text = "Double-click color value to change";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(321, 450);
+            this.Controls.Add(this.InstructionLabel);
+            this.Controls.Add(this.WriteFileButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ReadSCTButton);
+            this.Name = "Form1";
+            this.Text = "SCT File Color Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sCTdefineBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ReadSCTButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource sCTdefineBindingSource;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.Button WriteFileButton;
+        private System.Windows.Forms.Label InstructionLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +16,169 @@ namespace SCTColorManager
 {
     public partial class Form1 : Form
     {
+        public static DataTable Clr = new DataSet1.SCTdefineDataTable();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int RGB2Decimal (int R, int G, int B)
+        {
+            return R * 65536 + G * 256 + B;
+        }
+
+        private void LoadGridView()
+        {
+            string HC; Color color;
+            dataGridView1.DataSource = Clr;
+            dataGridView1.BackgroundColor = Color.Black;
+            dataGridView1.Columns[1].HeaderText = "Value";
+            foreach (DataGridViewRow dgvr in dataGridView1.Rows)
+            {
+                HC = Convert.ToInt32(dgvr.Cells["DecValue"].Value).ToString("X");
+                color = HexStringToColor(HC);
+                dgvr.DefaultCellStyle.ForeColor = color;
+                dgvr.DefaultCellStyle.BackColor = HexStringToColor("202020");
+            }
+
+        }
+
+        public static Color HexStringToColor(string hexColor)
+        {
+            string hc = ExtractHexDigits(hexColor);
+            if (hc.Length != 6)
+            {
+                // you can choose whether to throw an exception
+                //throw new ArgumentException("hexColor is not exactly 6 digits.");
+                return Color.Empty;
+            }
+            string r = hc.Substring(0, 2);
+            string g = hc.Substring(2, 2);
+            string b = hc.Substring(4, 2);
+            Color color;
+            try
+            {
+                int ri = Int32.Parse(r, NumberStyles.HexNumber);
+                int gi = Int32.Parse(g, NumberStyles.HexNumber);
+                int bi = Int32.Parse(b, NumberStyles.HexNumber);
+                color = Color.FromArgb(ri, gi, bi);
+            }
+            catch
+            {
+                // you can choose whether to throw an exception
+                //throw new ArgumentException("Conversion failed.");
+                return Color.Empty;
+            }
+            return color;
+        }
+
+        public static string ExtractHexDigits(string input)
+        {
+            // remove any characters that are not digits (like #)
+            var isHexDigit
+                = new Regex("[abcdefABCDEF\\d]+", RegexOptions.Compiled);
+            string newnum = "";
+            foreach (char c in input)
+            {
+                if (isHexDigit.IsMatch(c.ToString()))
+                {
+                    newnum += c.ToString();
+                }
+            }
+            return newnum;
+        }
+
+        private void WriteFileButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog
+            {
+                Title = "Save text file of #define lines",
+                InitialDirectory = "D:\\OneDrive\\Documents\\vFE_Files\\Resources\\",
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                RestoreDirectory = true,
+            };
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFile.FileName))
+                {
+                    foreach(DataRow dr in Clr.Rows)
+                    {
+                        sw.WriteLine("#define " + dr["Name"].ToString() + " " + dr["DecValue"].ToString());
+                    }
+                }
+            }
+        }
+
+        private void ReadDefineText()
+        {
+            string path; string define; string Name; int ClrValue;
+            string ToParse;
+            ;
+            OpenFileDialog openFile = new OpenFileDialog
+            {
+                Title = "Open text file of #define lines",
+                InitialDirectory = "D:\\OneDrive\\Documents\\vFE_Files\\Resources\\",
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                RestoreDirectory = true,
+            };
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                path = openFile.FileName;
+                string[] Lines = File.ReadAllLines(path);
+
+                foreach (string Line in Lines)
+                {
+                    ToParse = Line;
+                    define = ToParse.Substring(0, ToParse.IndexOf(" ")).Trim();
+                    if (define.IndexOf("#define") != -1)
+                    {
+                        ToParse = ToParse.Substring(ToParse.IndexOf(" ") + 1);
+                        Name = ToParse.Substring(0, ToParse.IndexOf(" ")).Trim();
+                        ToParse = ToParse.Substring(ToParse.IndexOf(" ") + 1);
+                        ClrValue = Convert.ToInt32(ToParse);
+
+                        Clr.Rows.Add(Name, ClrValue);
+                    }
+                }
+                Debug.WriteLine("Table has " + Clr.Rows.Count);
+            }
+        }
+
+        private void ReadSCTButton_Click(object sender, EventArgs e)
+        {
+            ReadDefineText();
+            LoadGridView();
+        }
+
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ColorDialog cd = colorDialog1;
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                DataGridViewRow dgr = dataGridView1.CurrentRow;
+                dgr.DefaultCellStyle.ForeColor = cd.Color;
+                Debug.WriteLine(RGB2Decimal(cd.Color.R, cd.Color.G, cd.Color.B));
+                dgr.Cells[1].Value = RGB2Decimal(cd.Color.R, cd.Color.G, cd.Color.B);
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ColorDialog cd = colorDialog1;
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                DataGridViewRow dgr = dataGridView1.CurrentRow;
+                dgr.DefaultCellStyle.ForeColor = cd.Color;
+                Debug.WriteLine(RGB2Decimal(cd.Color.R, cd.Color.G, cd.Color.B));
+                dgr.Cells[1].Value = RGB2Decimal(cd.Color.R, cd.Color.G, cd.Color.B);
+            }
         }
     }
 }
